d9086f33eb7b32fc83400b2d085e736ac4ac78f7
Leo-SLC <leoantinozzi@Leos-MacBook-Pro.local>
2023-09-13 15:51:45 -0600

    Initial Code Push

---
+++ static/js/logic.js
@@ -0,0 +1,20 @@
+var map = L.map('map').setView([0,-40], 3);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+d3.json(url)
+
+L.geoJSON(data, {
+    style: function (feature) {
+        return {color: feature.properties.color};
+    }
+}).bindPopup(function (layer) {
+    return layer.feature.properties.description;
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
