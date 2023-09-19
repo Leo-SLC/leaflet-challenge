81ca5066da19046b18527ce3bfb17138e799c41f
Leo-SLC <leoantinozzi@Leos-MacBook-Pro.local>
2023-09-18 21:04:21 -0600

    Code Push

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,16 +5,50 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-d3.json(url)
+d3.json(url).then(data=>{
+    console.log(data.features[100])
+    L.geoJSON(data, {
+       pointToLayer: function(geoJsonPoint, latlng) {
+        return L.circleMarker(latlng);
+    },
+        style: function (feature) {
+            let mag = feature.properties.mag*4;
+            let depth = feature.geometry.coordinates[2];
 
-L.geoJSON(data, {
-    style: function (feature) {
-        return {color: feature.properties.color};
-    }
-}).bindPopup(function (layer) {
-    return layer.feature.properties.description;
-}).addTo(map);
+            return {
+                radius: mag,
+                fillOpacity: .65,
+                weight: 0.8,
+                color: 'black',
+                fillColor: 
+                    depth<10 ? 
+                    'green' : 
+                    depth<50 ?
+                    'yellow' : 'red' 
+            };
+        }
+    }).bindPopup(function ({feature}) {
+        let mag = feature.properties.mag;
+        let place = feature.properties.place
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+        return `<h3>${place}<br>Magnitude: ${mag}</h3>`;
+    }).addTo(map);
+});
+
+var legend = L.control({position: 'bottomright'});
+    legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend');
+
+            div.innerHTML += `
+                <div style="background:green;color:white;padding:5px"><10</div>
+                <div style="background:yellow;color:black;padding:5px">50</div>
+                <div style="background:red;color:white;padding:5px">90+</div>
+            `
+
+       
+    return div;
+};
+
+legend.addTo(map);
+
