9a02846ee98665fb349c37f5ac3a66e079dc9b23
Leo-SLC <leoantinozzi@Leos-MacBook-Pro.local>
2023-09-27 15:56:38 -0600

    Pushing Update

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,8 @@
 var map = L.map('map').setView([0,-40], 3);
 
+let techtonicplates = new L.layerGroup()
+let earthquakes = new L.layerGroup()
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
@@ -22,9 +25,16 @@ d3.json(url).then(data=>{
                 color: 'black',
                 fillColor: 
                     depth<10 ? 
-                    'green' : 
+                    'green' :
+                    depth<30 ?
+                    'lime' :
                     depth<50 ?
-                    'yellow' : 'red' 
+                    'yellow' :
+                    depth<70 ?
+                    'peachpuff' :
+                    depth<90 ?
+                    'orange' : 'red'  
+
             };
         }
     }).bindPopup(function ({feature}) {
@@ -32,8 +42,26 @@ d3.json(url).then(data=>{
         let place = feature.properties.place
 
         return `<h3>${place}<br>Magnitude: ${mag}</h3>`;
-    }).addTo(map);
+    }).addTo(earthquakes);
 });
+
+
+d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(data=>{
+    console.log(data)
+    L.geoJSON(data,{
+        color: 'orange',
+        weight: 2,
+
+    }).addTo(techtonicplates);
+})
+
+// L.control.layers(
+//     {},{overlays:{
+//         "Techtonic Plates":techtonicplates,
+//         "Earthquakes":earthquakes
+//     }}).addTo(map);
+
+L.control.layers({},{techtonicplates,earthquakes}).addTo(map);
 
 var legend = L.control({position: 'bottomright'});
     legend.onAdd = function (map) {
@@ -41,8 +69,11 @@ var legend = L.control({position: 'bottomright'});
     var div = L.DomUtil.create('div', 'info legend');
 
             div.innerHTML += `
-                <div style="background:green;color:white;padding:5px"><10</div>
-                <div style="background:yellow;color:black;padding:5px">50</div>
+                <div style="background:green;color:white;padding:5px">-10 -10</div>
+                <div style="background:lime;color:black;padding:5px">10 - 30</div>
+                <div style="background:yellow;color:black;padding:5px">30 - 50</div>
+                <div style="background:peachpuff;color:black;padding:5px">50 - 70</div>
+                <div style="background:orange;color:black;padding:5px">70 - 90</div>
                 <div style="background:red;color:white;padding:5px">90+</div>
             `
 
